66b061f3a91a30381b0b1cf9214a931e22c7edb5
gergohrubo <gergohrubo@gmail.com>
2019-12-04 17:27:24 +0100

    Reset again

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const db = new Sequelize(databaseUrl)
 //when updating rows in the database replace this for one-time use: db.sync({ force: true })
 //delete it after one time use otherwise tables will be cleared at each server re-start
 //same thing works for the remote database on heroku: commit and push with force: true and then without it, and that resets the database
-db.sync()
+db.sync({ force: true })
   .then(() => console.log('this is my football-api'))
   .catch(console.error)
 
